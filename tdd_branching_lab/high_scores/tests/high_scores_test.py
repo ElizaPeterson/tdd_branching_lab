12e3d66e90907a1aceea97616a34c6d1499a7acb
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-01-19 15:42:24 +0000

    first two problems solved

--- tdd_branching_lab/high_scores/tests/high_scores_test.py
+++ tdd_branching_lab/high_scores/tests/high_scores_test.py
@@ -6,15 +6,19 @@ from src.high_scores import latest, personal_best, personal_top_three
 
 
 class HighScoresTest(unittest.TestCase):
-    
+
+    high_scores=[1, 2, 3, 4, 5]
     # Tests
 
     # Test latest score (the last thing in the list)
-
+    def test_latest_score(self):
+        self.assertEqual(5, latest(self.high_scores))
     # Test personal best (the highest score in the list)
+    def test_highest_score(self):
+        self.assertEqual(5, personal_best(self.high_scores))
 
     # Test top three from list of scores
-
+    
     # Test ordered from highest tp lowest
 
     # Test top three when there is a tie
